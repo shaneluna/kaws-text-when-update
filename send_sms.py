0a71ebe6e17d0c93d354ea4f560cebb863207638
Shane <shaneluna95@gmail.com>
2021-05-16 11:08:24 -0700

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -3,16 +3,16 @@
 from twilio.rest import TwilioRestClient
 
 # Find these values at https://twilio.com/user/account
-account_sid = "AC23649d11f4161e2f891e8e334c50115e"
-auth_token = "14014f970573d3028453e7cf70181576"
+account_sid = ""
+auth_token = ""
 client = TwilioRestClient(account_sid, auth_token)
 
 twilioPhone = "+14083350434"
 
 #Shane
-message = client.messages.create(to="+14083093360", from_=twilioPhone,
+message = client.messages.create(to="+1xxxxxxxxxx", from_=twilioPhone,
                                      body="UPDATED!!!\nhttp://kawsone.com/\n...\nGO!!")
 
 #Rey
-message2 = client.messages.create(to="+16199485952", from_=twilioPhone,
+message2 = client.messages.create(to="+1xxxxxxxxxx", from_=twilioPhone,
                                      body="UPDATED!!!\nhttp://kawsone.com/\n...\nGO!!")
